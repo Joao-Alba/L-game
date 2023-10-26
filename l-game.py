d891e7f3acf3d18a864fb919d4346e9fb5292433
João Arthur Pereira Alba <jalba@furb.br>
2023-10-26 20:10:34 -0300

    adicionada base

--- l-game.py
+++ l-game.py
@@ -1,21 +1,61 @@
 import pygame
 import sys
+from enum import Enum
 
 # Initialize Pygame
 pygame.init()
 
 # Set up display
 screen = pygame.display.set_mode((500, 400))
-pygame.display.set_caption("Grid Game")
+pygame.display.set_caption("L-GAME")
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 
+# Block directions
+class Direction(Enum):
+    VERTICAL = 1
+    VERTICAL_MIRROR_X = 2
+    VERTICAL_MIRROR_Y = 3
+    VERTICAL_MIRROR_X_Y = 4
+    HORIZONTAL = 5
+    HORIZONTAL_MIRROR_X = 6
+    HORIZONTAL_MIRROR_Y = 7
+    HORIZONTAL_MIRROR_X_Y = 8
+
 # Define grid properties
 GRID_SIZE = 4
 CELL_SIZE = 100
+
+# Images
+img_red_square = pygame.image.load("resources/red_square.png").convert()
+img_blue_square = pygame.image.load("resources/blue_square.png").convert()
+
+# Game control variables
+game_state = GameState.RED_TO_MOVE_BLOCK
+
+red_player_position = {"start": {0, 1}, "direction": Direction.HORIZONTAL_MIRROR_Y}
+blue_player_position = {"start": {3, 2}, "direction": Direction.HORIZONTAL_MIRROR_X}
+
+class GameState(Enum):
+    RED_TO_MOVE_BLOCK = "Vermelho mexe L"
+    RED_TO_MOVE_COIN = "Vermelho mexe moeda"
+    BLUE_TO_MOVE_BLOCK = "Azul mexe L"
+    BLUE_TO_MOVE_COIN = "Azul mexe moeda"
+
+def advanceState():
+    match game_state:
+        case GameState.RED_TO_MOVE_BLOCK:
+            game_state = GameState.RED_TO_MOVE_COIN
+        case GameState.RED_TO_MOVE_COIN:
+            game_state = GameState.BLUE_TO_MOVE_BLOCK
+        case GameState.BLUE_TO_MOVE_BLOCK:
+            game_state = GameState.BLUE_TO_MOVE_COIN
+        case GameState.BLUE_TO_MOVE_COIN:
+            game_state = GameState.RED_TO_MOVE_BLOCK
+
 
 # Function to draw the grid
 def draw_grid():
@@ -32,6 +72,12 @@ def draw_buttons():
     for i, color in enumerate([WHITE, GRAY, BLACK]):
         pygame.draw.rect(screen, color, (GRID_SIZE * CELL_SIZE + button_padding, i * (button_height + button_padding), button_width, button_height))
 
+def click(cell_x, cell_y):
+    print(str(cell_x) + ' - ' + str(cell_y))
+    paintCell(cell_x, cell_y, img_red_square)
+
+def paintCell(cell_x, cell_y, img)
+    
 # Main game loop
 while True:
     for event in pygame.event.get():
@@ -54,8 +100,7 @@ while True:
             else:
                 grid_x = x // CELL_SIZE
                 grid_y = y // CELL_SIZE
-                # Grid cell clicked
-                print(f"Grid cell ({grid_x}, {grid_y}) clicked")
+                click(grid_x, grid_y)
 
     screen.fill(WHITE)
     draw_grid()
